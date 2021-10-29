e84bf15eda2e901e447caec984df24dc88914655
ivy <61070075@kmitl.ac.th>
2021-10-29 22:16:47 +0700

    Start Project
    Add nav bar  and content

---
+++ Myproject/home/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.template import RequestContext
+# Create your views here.
+def home(request):
+    context = {'namae' : "Thanawath Petavibornsatarn",
+        'age' : '15',
+        'bd' : '20/04/2012'
+    }
+    return render(request, 'index.html', context)
+
+def page1(request):
+    return render(request, 'page1.html')
