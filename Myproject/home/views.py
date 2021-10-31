89815811f14156fd1353f9c5ed2bb4fb631c887a
ivy <61070075@kmitl.ac.th>
2021-11-01 00:47:19 +0700

    Add card home page and add footer

--- Myproject/home/views.py
+++ Myproject/home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.template import RequestContext
 import random as r
+from django.template import Context, Template
 # Create your views here.
 def home(request):
     context = {'namae' : "Thanawath Petavibornsatarn",
@@ -13,7 +14,13 @@ def home(request):
 def page1(request):
     return render(request, 'page1.html')
 
+
 def randompage(request):
     random_number = r.randrange(100000, 1000000)
-    
-    return render(request, 'randompage.html', {'random_number' : random_number})
+    return render(request, 'randompage.html', {
+        'random_number' : random_number,
+        'two_num_last' : str(random_number)[4:],
+        'three_num_first' : str(random_number)[:3],
+        'three_num_last' : str(random_number)[3:],
+        'test' : ""
+        })
