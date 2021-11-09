94b99fcdceb2fe38553033c5e3ad2e22f5edbe08
eartsvnh <64070236@kmitl.ac.th>
2021-11-09 19:11:54 +0700

    add logo
    เพิ่มโลโก้แต่จะกลับมาแก้ใหม่

--- Myproject/Myproject/settings.py
+++ Myproject/Myproject/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
+MEDIA_URL = '/images/'
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-)
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
